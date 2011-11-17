fa384d191eba4b268cd15fd99e06806dce6a08eb
jari <jari.bjorne@utu.fi>
2011-11-17 16:50:16 +0200

    Tried again to delete BIGraph/apps

--- CommonUtils/BIGraph/apps/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-User-interface functions for several commonly needed tasks.
-
-Each module contains optparse-based function 'interface' that can be used as
-such. By default, the function reads the command-line arguments from
-sys.argv[1:] but arguments can also be manually specified.
-"""
